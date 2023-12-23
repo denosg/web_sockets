37650a8ba5d82ce4031413afc83b3860fc4fbf5f
denos <costelasdenis@gmail.com>
2023-12-23 17:35:18 +0200

    web sockets connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path')
+const http = require('http')
 const appRoute = require('./routers/app_router')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname, '../public')
@@ -10,8 +14,13 @@ const publicDirPath = path.join(__dirname, '../public')
 //Routes
 app.use(express.static(publicDirPath))
 app.use('/', appRoute);
-app.listen(port, () => {
+
+io.on('connection', () => {
+    console.log(`New WebSocket connection`);
+  });
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
 
-module.exports = app
+module.exports = server
