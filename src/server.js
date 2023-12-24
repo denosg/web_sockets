8aad9ebf17570865f6401c12103df00b2301bb00
denos <costelasdenis@gmail.com>
2023-12-24 19:35:17 +0200

    client side

--- src/server.js
+++ src/server.js
@@ -15,8 +15,20 @@ const publicDirPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirPath))
 app.use('/', appRoute);
 
-io.on('connection', () => {
+let count = 0
+
+// server (emit) -> client (receive) - countUpdated
+// server (emit) -> server (receive) - increment
+
+io.on('connection', (socket) => {
     console.log(`New WebSocket connection`);
+
+    socket.emit('countUpdated', count)
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count) // for one person
+        io.emit('countUpdated', count) // for everyone
+    })
   });
 
 server.listen(port, () => {
